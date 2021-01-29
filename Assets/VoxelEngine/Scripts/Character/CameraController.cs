66ad9792874b6e729e8b8e32ac1f36374cb397fa
Tony Li <tonyli99@users.noreply.github.com>
2021-01-28 19:54:07 -0500

    Updated player & camera prefabs to work w/VoxelEngine

--- Assets/VoxelEngine/Scripts/Character/CameraController.cs
+++ Assets/VoxelEngine/Scripts/Character/CameraController.cs
@@ -13,6 +13,7 @@ public class CameraController : MonoBehaviour
     void Awake()
     {
         cameraTransform = transform;
+        if (target == null) target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Start()
@@ -23,8 +24,13 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
+        // Get joystick look axes and correct for drift:
+        var rightStickX = Input.GetAxisRaw("Right Stick X");
+        var rightStickY = Input.GetAxisRaw("Right Stick Y");
+        if (Mathf.Abs(rightStickX) < 0.05f) rightStickX = 0;
+        if (Mathf.Abs(rightStickY) < 0.05f) rightStickY = 0;
+        var rightStickInput = sensitivity * new Vector2(rightStickX, rightStickY);
         var mouseInput = sensitivity * new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        var rightStickInput = sensitivity * new Vector2(Input.GetAxisRaw("Right Stick X"), Input.GetAxisRaw("Right Stick Y"));
         //input = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         //input *= sensitivity;
         input = mouseInput + rightStickInput;
