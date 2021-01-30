3af76e8b1f3373001a00caaf740e68f7a6f0b82c
turbonabi <turbonabi@hotmail.com>
2021-01-30 18:01:06 -0500

    spotlight beam tilt

--- Assets/__Game/Scripts/Guards/Aliens.cs
+++ Assets/__Game/Scripts/Guards/Aliens.cs
@@ -32,11 +32,17 @@
         float targetHeight;
         Vector3 tempPosition;
         Vector3 sceneCenter;
+
+        DevSpotLightControl spotlightcontrol;
+        Quaternion spotlightTargetRot;
+        float lightMoveSpan;
         void Start()
         {
             rb = GetComponent<Rigidbody>();
             targetHeight = transform.position.y;
             sceneCenter = new Vector3(32, targetHeight, 32);
+            spotlightcontrol = GetComponentInChildren<DevSpotLightControl>();
+            spotlightTargetRot = spotlightcontrol.transform.rotation;
         }
 
         private void Update()
@@ -64,12 +70,11 @@
             Vector3 targetDirection = sceneCenter - transform.position;
             targetDirection.y = 0;
             
-            if (lastMoveTime + currentMoveTimeInterval < Time.time)
+            if (lastMoveTime + currentMoveTimeInterval < Time.time || targetDirection.magnitude > 35)
             {
-                Debug.Log("targetDirection = " + targetDirection.magnitude);
                 lastMoveTime = Time.time;
                 currentMoveTimeInterval = averageMoveTimeInterval + timeIntervalVariance * (Random.value - 0.5f);
-
+                lightMoveSpan = currentMoveTimeInterval;
                 float moveForce = Random.value * maxMoveForce;
                 //Check boundaries
                 int directionMinRange = 0;
@@ -78,6 +83,7 @@
                 {
                     directionMinRange = -45;
                     directionMaxRange = 45;
+                    moveForce = maxMoveForce;
                 }
                 else
                 {
@@ -85,11 +91,20 @@
                 }
                 Vector3 moveDir = Quaternion.Euler(0, Random.Range(directionMinRange, directionMaxRange), 0) * targetDirection.normalized;
                 rb.AddForce(moveDir * moveForce, ForceMode.Acceleration);
+
+                MoveSpotLight();
             }
             if (rb.velocity.magnitude >= 0.0001f)
             {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rb.velocity), Time.deltaTime * 2.5f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rb.velocity), (1 - (lightMoveSpan / currentMoveTimeInterval)) * 2);
             }
+            spotlightcontrol.transform.localRotation = Quaternion.Lerp(spotlightcontrol.transform.localRotation, spotlightTargetRot, 1 - (lightMoveSpan/currentMoveTimeInterval));
+            lightMoveSpan -= Time.deltaTime;
+        }
+
+        void MoveSpotLight()
+        {
+            spotlightTargetRot.eulerAngles = new Vector3(Random.Range(-45f, 0), 0, 0);
         }
 
         private void OnTriggerEnter(Collider other)
