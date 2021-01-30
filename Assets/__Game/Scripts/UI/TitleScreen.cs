247763ed05e0867cbcce03cdbb1657edbf687542
Tony Li <tonyli99@users.noreply.github.com>
2021-01-30 14:17:23 -0500

    Title scene

---
+++ Assets/__Game/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿namespace Game
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class TitleScreen : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.anyKeyDown || Input.GetButtonDown("Fire1"))
+            {
+                GetComponent<Animator>().Play("FadeToBlack");
+                Invoke(nameof(LoadGameplayScene), 0.6f);
+            }
+        }
+
+        private void LoadGameplayScene()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
